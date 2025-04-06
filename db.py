03929cffd5816312e3391ae2b6f1c75d73e848d0
azeem30 <pinjariashpak3@gmail.com>
2025-04-06 14:23:13 +0530

    Configured to connect to deployed database

--- db.py
+++ db.py
@@ -7,17 +7,17 @@ load_dotenv()
 def get_db_connection():
     try:
         connection = pymysql.connect(
-            host=os.getenv('DB_HOST'),  
-            user=os.getenv('DB_USER'),   
+            host=os.getenv('DB_HOST'),
+            user=os.getenv('DB_USER'),
             password=os.getenv('DB_PASSWORD'),
             database=os.getenv('DB_NAME'),
-            port=int(os.getenv('DB_PORT', '3306')), 
+            port=3306,
             cursorclass=pymysql.cursors.DictCursor,
-            ssl=None, 
+            ssl=None,
             connect_timeout=10  
         )
-        print("✅ Connected to Google Cloud MySQL database.")
+        print("✅ Connected to Render MySQL database!")
         return connection
     except pymysql.MySQLError as e:
-        print(f"❌ Error while connecting to Google Cloud MySQL: {e}")
+        print(f"❌ Error connecting to Render MySQL: {e}")
         return None
